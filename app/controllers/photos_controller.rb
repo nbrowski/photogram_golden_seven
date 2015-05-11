a19c3ef414861687fb75a285b76bf6477c1bceb9
nbrowski <nbrower@dhcp-10-105-240-90.wireless.northwestern.private>
2015-05-11 11:35:10 -0500

    Completed
    Also added Back, Edit, Delete links to the Show page

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,12 +2,41 @@ class PhotosController < ApplicationController
   def index
     @list_of_photos = Photo.all
   end
+
   def show
     photo_id=params[:id]
     @photo_selected=Photo.find_by(:id => photo_id)
   end
-  def new_form
 
+  def new_form
   end
 
+  def create_photo
+    p=Photo.new
+    p.source=params[:the_source]
+    p.caption=params[:the_caption]
+    p.save
+
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def destroy
+    p_to_destroy=Photo.find_by(:id => params[:id])
+    p_to_destroy.destroy
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def edit_form
+    @photo_to_edit=Photo.find_by(:id => params[:id])
+  end
+
+  def update_row
+    p_to_update=Photo.find_by(:id => params[:id])
+    p_to_update.caption=params[:the_caption]
+    p_to_update.source=params[:the_source]
+    p_to_update.save
+
+    redirect_to("http://localhost:3000/photos")
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "photos", :action => "index" })
 
-  # Routes to READ photos
-  get("/photos",           { :controller => "photos", :action => "index" })
-  get("/photos/new",       { :controller => "photos", :action => "new_form" })
-  get("/photos/:id",       { :controller => "photos", :action => "show" })
+
+  get("/photos",              { :controller => "photos", :action => "index" })
+  get("/photos/new",          { :controller => "photos", :action => "new_form" })
+  get("/create_photo",  { :controller => "photos", :action => "create_photo" })
+
+  #Routes with variables
+  get("/photos/:id",          { :controller => "photos", :action => "show" })
+  get("/delete_photo/:id",    { :controller => "photos", :action => "destroy" })
+  get("/photos/:id/edit",     { :controller => "photos", :action => "edit_form" })
+  get("/update_photo/:id",    { :controller => "photos", :action => "update_row" })
 end
